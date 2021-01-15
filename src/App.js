387580f2ed6c78cd92be4409c49f572f827a9750
bonasunu <bonaventurasunu@gmail.com>
2021-01-15 18:25:20 +0700

    build function for wand selection

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import axios from 'axios'
+import { Container, Button } from 'reactstrap'
 
 function App() {
+  let wands
+  axios
+    .get('http://hp-api.herokuapp.com/api/characters')
+    .then((data) => {
+      wands = data.data.map((i) => i['wand'])
+      console.log(wands[0]['wood'])
+      return wands
+    })
+    .catch((e) => console.log(e))
+
+  function handleGenerateWand() {
+    let wandIndex = Math.floor(Math.random() * 24 + 1)
+
+    console.log(wands[wandIndex])
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Container className="p-3 text-center">
+        <h1>Generate Your Wand</h1>
+        <Button onClick={handleGenerateWand}>Collect Now</Button>
+      </Container>
+    </>
+  )
 }
 
-export default App;
+export default App
---
+++ src/GeneratePdf.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function GeneratePdf() {
+  return (
+    <div>
+      <p>PDF</p>
+    </div>
+  )
+}
+
+export default GeneratePdf
